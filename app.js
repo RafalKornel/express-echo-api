10303056192314cf39b2ef9526606e9b01be41df
RafalKornel <rafal.kornel123@gmail.com>
2024-03-05 16:11:44 +0100

    fixes

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var port = process.env.PORT || 3000,
-    ip   = process.env.IP   || '0.0.0.0';
+  ip = process.env.IP || "0.0.0.0";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, ip, function () {
-  console.log('Echo API is listening on port ' + port );
+  console.log("Echo API is listening on port " + port);
 });
 
-var reqData = function(req) {
-  const params = ['method', 'hostname', 'path', 'query', 'headers', 'body']
-  return params.reduce(
-    (accumulator, currentValue) => { accumulator[currentValue] = req[currentValue]; return accumulator},
-    {}
-  );
+var reqData = function (req) {
+  debugger;
+
+  const params = ["method", "hostname", "path", "query", "headers", "body"];
+  return params.reduce((accumulator, currentValue) => {
+    accumulator[currentValue] = req[currentValue];
+    return accumulator;
+  }, {});
 };
 
-app.all('*', function (req, res) {
-  res.set('Content-Type', 'application/json');
+app.all("*", function (req, res) {
+
+  console.log(req.params);
+  console.log(req.params.dictionary);
+  console.log(req.body);
+  res.set("Content-Type", "application/json");
   res.set("Access-Control-Allow-Origin", "*");
   var response = reqData(req);
 
-  var stringifiedResponse = JSON.stringify(response,null,2);
+  var stringifiedResponse = JSON.stringify(response, null, 2);
   console.log(stringifiedResponse);
-  
+
   res.status(200).send(stringifiedResponse);
 });
 
